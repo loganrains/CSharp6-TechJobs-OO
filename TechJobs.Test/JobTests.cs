370a0454967cd5e8b2e40fe0bde6193fba27e3c8
loganrains <logankrains@gmail.com>
2024-06-01 14:07:58 -0500

    Created Test Method for Setting Job ID, not complete but passing

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -6,11 +6,18 @@ namespace TechJobs.Tests
 	{
         //Testing Objects
         //initalize your testing objects here
+        //Testing objects
+        Job job1 = new Job();
+
+        Job job2 = new Job();
+
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
         [TestMethod]
-        public void TestMethod()
-        {
-            //TODO: Task 4: remove this method before you create your first test method 
+        public void TestSettingJobId() {
+            Assert.AreNotEqual(job1.Id, job2.Id);
         }
     }
 }
