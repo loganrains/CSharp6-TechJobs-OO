5db636295c3a441e8e266ec7d6ecfb46c8038b2d
loganrains <logankrains@gmail.com>
2024-06-14 10:59:37 -0500

    Created JobField, Refactored Employer, Location, Competency, & Position Classe, Updated Test w/ ID so they can pass when ran seperately

---
+++ TechJobsOOAutoGraded6/JobField.cs
@@ -0,0 +1,39 @@
+﻿namespace TechJobsOOAutoGraded6;
+
+public abstract class JobField
+{
+    // list reoccuring fields - ID, Next ID, Value
+    // list reoccuring constructors - ID, Value
+    // list reocuring custom methods - Equals, hashcode, ToString
+
+    public int Id { get; }
+    private static int nextId = 1;
+    public string Value { get; set; }
+
+    public JobField()
+    {
+        Id = nextId;
+        nextId++;
+    }
+
+    public JobField(string value) : this()
+    {
+        Value = value;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is JobField jobField &&
+               Id == jobField.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id);
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
